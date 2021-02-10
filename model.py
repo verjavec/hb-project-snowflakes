aacee700fe0195f77ff1e70b42c6a9ba8c8e77e6
Vicki Erjavec <verjavec@gmail.com>
2021-02-10 08:10:04 +0000

    User database seeding and access complete

---
+++ model.py
@@ -0,0 +1,90 @@
+""" Models for snowflake patterns """
+
+from flask_sqlalchemy import SQLAlchemy
+import datetime
+
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    """A user."""
+
+    __tablename__ = 'users'
+
+    user_id = db.Column(db.Integer,
+                        autoincrement=True,
+                        primary_key=True)
+    name = db.Column(db.String)
+    email = db.Column(db.String, unique=True)
+    password = db.Column(db.String)
+
+    def __repr__(self):
+        return f'<User user_id={self.user_id} name={self.name} email={self.email}>'
+
+
+class Pattern(db.Model):
+    """A pattern."""
+
+    __tablename__ = 'patterns'
+
+    pattern_id = db.Column(db.Integer,
+                        autoincrement=True,
+                        primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
+    completion_date = db.Column(db.DateTime)
+    num_rounds = db.Column(db.Integer)
+    branch = db.Column(db.Integer)
+    point = db.Column(db.Integer)
+    round_id = db.Column(db.Integer, db.ForeignKey('rounds.round_id'))
+
+    round = db.relationship('Round', backref='patterns')
+    user = db.relationship('User', backref='patterns')
+
+    def __repr__(self):
+        return f'<Pattern pattern_id={self.pattern_id} num_rounds={self.num_rounds}>'
+
+class Round(db.Model):
+    """A round."""
+
+    __tablename__ = 'rounds'
+
+    round_id = db.Column(db.Integer,
+                        autoincrement=True,
+                        primary_key=True)
+    start_seq = db.Column(db.String)
+    end_seq = db.Column(db.String)
+    repeater_id = db.Column(db.Integer, db.ForeignKey('repeaters.repeater_id'))
+
+    repeater = db.relationship('Repeater', backref='rounds')
+
+    def __repr__(self):
+        return f'<Round round_id={self.round_id} start_seq={self.start_seq}>'
+
+class Repeater(db.Model):
+    """A user."""
+
+    __tablename__ = 'repeaters'
+
+    repeater_id = db.Column(db.Integer,
+                        autoincrement=True,
+                        primary_key=True)
+    sequence = db.Column(db.Text)
+    round_no = db.Column(db.Integer)
+
+    def __repr__(self):
+        return f'<Round round_id={self.round_id} start_seq={self.start_seq}>'
+
+
+def connect_to_db(flask_app, db_uri='postgresql:///snowflakes', echo=True):
+    flask_app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+    flask_app.config['SQLALCHEMY_ECHO'] = echo
+    flask_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.app = flask_app
+    db.init_app(flask_app)
+
+    print('Connected to the db!')
+
+
+if __name__ == '__main__':
+    from server import app
